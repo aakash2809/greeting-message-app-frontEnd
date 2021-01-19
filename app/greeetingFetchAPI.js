89097fec2a2f9aeaa6942130fad4949364f26622
aakash2809 <aakashrajak289@gmail.com>
2021-01-19 08:37:17 +0530

    add: scroll bar with panel

--- app/greeetingFetchAPI.js
+++ app/greeetingFetchAPI.js
@@ -7,30 +7,4 @@ function getGreeting(){
        })
 }
 
-function getGreeting(){
-    url ='http://localhost:2000/allGreetings'
-    fetch(url).then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        console.log(data)
-       })
-}
-
-function postGreeting(){
-    url ='http://localhost:2000/allGreetings'
-    fetch(url).then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        console.log(data)
-       })
-}
-
-function deleteGreeting(){
-    url ='http://localhost:2000//greeting/:greetingId'
-    fetch(url).then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        console.log(data)
-       })
-}
 getData();
