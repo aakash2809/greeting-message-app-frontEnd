f9093ebce291033536f39f7a0e10b4064288452b
aakash2809 <aakashrajak289@gmail.com>
2021-01-20 06:58:26 +0530

    add: fetch api to get all existing greetings

--- app/greeetingFetchAPI.js
+++ app/greeetingFetchAPI.js
@@ -1,10 +1,23 @@
-function getGreeting(){
+
+/**
+ * Get all available greetings
+*/
+getAllGreetings = () =>{
     url ='http://localhost:2000/allGreetings'
     fetch(url).then((response)=>{
         return response.json();
     }).then((data)=>{
-        console.log(data)
-       })
+        const html = data.data.map(user=>{
+            return `<div class = "greeting-element">
+            <p>GreetingId:${user._id}</p>
+            <p>Name:${user.name}</p>
+            <p>Message:${user.message}</p>
+            <p>Created At:${user.createdAt}</p>
+           </div>` 
+        })
+               
+    document.querySelector("#app").insertAdjacentHTML("afterbegin", html); 
+    }).catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
 }
 
-getData();
+getAllGreetings();
