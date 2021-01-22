2e248baabc227f89d68a142d116e8eeda6bf0a3e
aakash2809 <aakashrajak289@gmail.com>
2021-01-22 10:11:10 +0530

    refactor: remove commented code, and add auto reload

--- app/greeetingFetchAPI.js
+++ app/greeetingFetchAPI.js
@@ -12,48 +12,18 @@ getAllGreetings = () => {
             <pre>Name:${greeting.name}</pre>
             <pre>Message:${greeting.message}</pre>
             <pre>Created At:${greeting.createdAt}</pre>
-            <button type="submit" class="btn">Edit</button>
-            <button type="submit" class="btn" onclick="alert('a')">Delete</button>
+            <button type="submit" class="btn" onclick='prompt()'>Edit</button>
+            <button type="submit" class="btn" onclick="confirm('Delete Greeting?')">Delete</button>
            </div>`
         })
         document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
     }).catch((err) => console.log(err));
 }
 
-/* postGreeting = () => {
-    document.getElementById('addGreeting').onsubmit = (e) => {
-        e.preventDefault();
-        const data = new URLSearchParams();
-        console.log(data )
-        for (const pair of new FormData(e.target)) {
-            console.log(pair); //console pairs of arrays
-            console.log(pair[1]);
-            
-            data.append(pair[1]);
-        }
-        console.log(e.target);//console actual form 
-        url = 'http://localhost:2000/addGreeting'
-        fetch(url, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify(data),
-        }).then((response) => {
-            console.log(response);
-            return response.json();
-        }).then((greetingData) => {
-            console.log(greetingData);
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
-} */
 
 postGreeting = () => {
     document.getElementById('addGreeting').addEventListener('submit', (e) => {
-        e.preventDefault();
+        
        var name = document.getElementById('name').value;
        var message = document.getElementById('msg').value;
        console.log(name);
