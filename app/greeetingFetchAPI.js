4a1524ff0c2d9226de961621b12cf116ddaff15b
aakash2809 <aakashrajak289@gmail.com>
2021-01-25 12:39:40 +0530

    add: comments and remove unnecessasry code

--- app/greeetingFetchAPI.js
+++ app/greeetingFetchAPI.js
@@ -1,5 +1,18 @@
+/**
+ * @module        app
+ * @file          greeetingFetchAPI.js
+ * @description   fetch APIs and implement them at fron end
+ * @author        Aakash Rajak <aakashrajak2809@gmail.com>
+*  @since         04/01/2021
+*------------------------------------------------------------------------------------------------------------------*/
+
 const serverUrl = 'http://localhost:2000';
 
+/**
+ * @description this function shows the popup after click on delete icon of each panel
+ * and it call to deleteGreeting() to delete greeting. 
+ * @param greetingId to delete greeting, appended with it
+*/
 delGreeting = (greetingId) => {
     document.querySelector('.delete-form-popup').style.display = 'flex';
     document.getElementById("deleteRecord").addEventListener('click',()=>{
@@ -7,6 +20,11 @@ delGreeting = (greetingId) => {
     });
 }
 
+/**
+ * @description this function shows the popup after click on edit icon of each panel
+ * and it call to updateGreeting() to update greeting. 
+ * @param greetingId to update greeting, appended with it
+*/
 putGreeting = (greetingId) => {
     document.querySelector('.update-form-popup').style.display = 'flex';
     console.log(document.getElementById("updateRecord"));
@@ -15,18 +33,30 @@ putGreeting = (greetingId) => {
     });
  }
 
+ /**
+ * @description to colse popup on click  close button of delete greeting form
+*/
 document.getElementById("closeDeleteGreetingForm").addEventListener('click', () => {
     document.querySelector('.delete-form-popup').style.display = 'none';
 })
 
+/**
+ * @description to colse popup on click  close button of update greeting form
+*/
 document.getElementById("closeUpdateGreetingForm").addEventListener('click', () => {
     document.querySelector('.update-form-popup').style.display = 'none';
 })
 
+/**
+ * @description to shows the popup 'add greeting form after' on click 'Add' (menu at sidebar)
+*/
 document.getElementById("addNewgreeting").addEventListener('click', () => {
     document.querySelector('.form-popup').style.display = 'flex';
   })
 
+/**
+ * @description to colse popup on click  close button of add greeting form
+*/
   document.getElementById("closeAddGreetingForm").addEventListener('click', () => {
     document.querySelector('.form-popup').style.display = 'none';
   })
@@ -81,6 +111,7 @@ postGreeting = () => {
 
 /**
  * @description delete greeting requested from greeting panel
+ * @param greetingId to delete greeting, appended with it
 */
 deleteGreeting = (greetingId) => {
     let id = greetingId;
@@ -103,6 +134,7 @@ deleteGreeting = (greetingId) => {
 
 /**
  * @description update greeting requested from greeting panel
+ * @param greetingId to update greeting, appended with it
 */
 updateGreeting = (greetingId) => {
     document.getElementById('updateGreeting').addEventListener('submit', (e) => {
